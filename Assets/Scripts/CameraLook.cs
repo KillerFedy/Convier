b5c58d107507a09a84b67d01fa0dd9ef8b6930c9
Фёдор <zuev.f.s@gmail.com>
2020-11-06 12:24:48 +1000

    Player Controller

---
+++ Assets/Scripts/CameraLook.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+    private float _mouseSensevity = 100f;
+    private float _xRotation = 0;
+    
+    private void Update()
+    {
+        float mouseY = Input.GetAxis("Mouse Y") * _mouseSensevity * Time.deltaTime;
+        _xRotation -= mouseY;
+        _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(_xRotation, 0, 0);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float _mouseSensevity = 100f;
+    private CharacterController _controller;
+    private float _speed = 5f;
+
+    private void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+    
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * _mouseSensevity * Time.deltaTime;
+        float moveZ = Input.GetAxis("Vertical") * _speed * Time.deltaTime;
+        float moveX = Input.GetAxis("Horizontal") * _speed * Time.deltaTime;
+        _controller.Move(transform.forward * moveZ + transform.right * moveX);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+}
